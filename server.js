633d553e4d70188a4ccb12304b70fd4d776b8037
Dylan Keyt <dylan-keyt@users.noreply.github.com>
2017-06-07 08:02:10 +1000

    feat(app): initial commit

---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require ('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  const log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server log.');
+    }
+  });
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render('maintenance.hbs', {
+    pageTitle: 'Maintenance Page',
+    welcomeMessage: 'We\'re fixing something. We\'ll be right back!',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website!',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+// app.get('/maintenance', (req, res) => {
+//   res.render('maintenance.hbs', {
+//     pageTitle: 'Maintenance Page',
+//     welcomeMessage: 'We\'ll be right back!',
+//     currentYear: new Date().getFullYear()
+//   });
+// });
+
+app.get('/bad', (req, res) => {
+  const error = {
+    errorMessage: 'Error handling request'
+  };
+  res.send(error);
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
